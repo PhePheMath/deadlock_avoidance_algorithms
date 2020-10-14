c2e89f2a12b08ef6cf72fb4b4efddf605854e502
PhePheMath <PhelyppeMath@outlook.com>
2020-10-13 21:23:30 -0300

    Banker's algorithm's logic

---
+++ bankeiro_compiler.py
@@ -0,0 +1,130 @@
+import curses
+
+
+class system_viewer():
+
+    def show_list(self, stdsrc, iterable, axis_y=1, axis_x=1, column_size=25,
+                  commands=False, pointer=-1, title='No Title'):
+        title_config = '|{:^'+str(
+            (len(iterable)//column_size)*len(iterable[0]) + len(iterable[0])-2
+            )+'}|'
+        title = title_config.format(title)
+
+        stdsrc.addstr(axis_y-1, axis_x, title)
+
+        for index, string in enumerate(iterable):
+            relative_column_position = (index//column_size)*len(string)
+            relative_row_position = (index % column_size)
+            y = axis_y+relative_row_position
+            x = axis_x+relative_column_position
+            # stdsrc.clear()
+            # stdsrc.addstr(1,1,f'x={x} y={y} command {commands}')
+            # stdsrc.refresh()
+            # stdsrc.getch()
+            stdsrc.addstr(y, x, string)
+            if commands and index == pointer:
+                stdsrc.addstr(y, x, string, curses.A_STANDOUT)
+        return x, y
+
+
+class bankeiro(system_viewer):
+
+    def __init__(self):
+        self.allocation = []  # list with allocated resources
+        self.max_need = []  # list with what processes need to completion
+        self.max_resources = ()  # tuple with the max of each resource
+
+    def get_info(self, text):
+        info = text.split('\n')
+        self.max_resources = tuple(map(int, info[0].split()))
+        self.allocation = [list(map(int, process.split()))
+                           for process in info[1:(len(info)//2)+1:]]
+        self.max_need = [tuple(map(int, process.split()))
+                         for process in info[(len(info)//2)+1::]]
+
+    def get_allocated(self):
+        allocated = [0, ]*len(self.max_resources)
+        for process in self.allocation:
+            for resource, number_allocations in enumerate(process):
+                allocated[resource] += number_allocations
+
+        return allocated
+
+    def get_available(self):
+        available = [0, ]*len(self.max_resources)
+        allocated = self.get_allocated()
+
+        for resource in range(len(self.max_resources)):
+            av = self.max_resources[resource] - allocated[resource]
+            available[resource] = av
+
+        return available
+
+    def get_remaining_need(self, number_process):
+        remaining_need = [0, ]*len(self.max_resources)
+
+        allocation = self.allocation[number_process]
+        max_need = self.max_need[number_process]
+
+        for resource in range(len(self.max_resources)):
+            rm = max_need[resource] - allocation[resource]
+            remaining_need[resource] = rm
+
+        return remaining_need
+
+    def find_adequate_process(self):
+        available = self.get_available()
+
+        for index in range(len(self.allocation)):
+            for resource in range(len(self.max_resources)):
+                still_need = self.get_remaining_need(index)
+                if still_need[resource] > available[resource]:
+                    break
+            else:
+                return index
+
+        return -1
+
+    def delete_process(self, number_process):
+        self.allocation.pop(number_process)
+        self.max_need.pop(number_process)
+
+    def run_banker_s_algorithm(self):
+        process = self.find_adequate_process()
+
+        if process > -1:
+            curses.wrapper(self.show_progress, process)
+
+        if process > -1:
+            self.delete_process(process)
+            print(f'Process {process} done!')
+            return True
+        elif not len(self.allocation):
+            print('Clear!!!')
+            return False
+        else:
+            print('DeadLock Found on banker\'s algorithm!!!')
+            return False
+
+    def show_progress(self, stdsrc, process):
+        stdsrc.clear()
+        show_processes = ['|{:^10} {:^9}|'.format(str(self.allocation[index]),
+                                                  str(self.max_need[index]))
+                          for index in range(len(self.allocation))]
+
+        self.show_list(stdsrc, show_processes,
+                       axis_y=3, axis_x=3,
+                       commands=True, pointer=process, title='Processes')
+
+        stdsrc.refresh()
+        stdsrc.getch()
+
+
+if __name__ == "__main__":
+    text = open('bankeiro_algorithm/allocations').read()
+    b = bankeiro()
+
+    b.get_info(text)
+    while True:
+        if not b.run_banker_s_algorithm():
+            break
