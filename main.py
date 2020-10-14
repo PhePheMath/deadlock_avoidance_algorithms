6ab7a34bb3f5d2480a379703a86147e1f52018f8
PhePheMath <PhelyppeMath@outlook.com>
2020-10-13 21:23:56 -0300

    Running banker's algorithm

---
+++ main.py
@@ -0,0 +1,20 @@
+import sys
+
+from bankeiro_compiler import bankeiro
+
+
+def main():
+    for source in sys.argv[1:]:
+        try:
+            text = open(source).read()
+            system = bankeiro()
+            system.get_info(text)
+            while True:
+                if not system.run_banker_s_algorithm():
+                    break
+        except FileNotFoundError as FNFE:
+            print('Source: ', FNFE)
+
+
+if __name__ == "__main__":
+    main()
